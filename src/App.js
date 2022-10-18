27ca338dd25eaf12ee653f20a402d4b428453a13
jieun-Seo <qhskdishfwkd@naver.com>
2022-10-18 11:33:35 +0900

    second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from "@fullcalendar/daygrid";
+
+//import "@fullcalendar/core/main.css";
+//import "@fullcalendar/daygrid/main.css";
+
+import "./custom.css";
 
 function App() {
+  const events = [{ title: "today's event", date: new Date() }];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div className="App">
+        <FullCalendar
+          defaultView="dayGridMonth"
+          header={{
+            left: "prev,next",
+            center: "title",
+            right: "dayGridMonth,timeGridWeek,timeGridDay"
+          }}
+          plugins={[dayGridPlugin]}
+          events={events}
+        />
+      </div>      
+    );
 }
 
 export default App;
